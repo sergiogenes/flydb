279e79a85d9a396bb527cb82c34a7ae573501c62
sergiogenes <genessergio@gmail.com>
2023-05-16 15:54:41 -0300

    Se cambió db/index

--- db/index.js
+++ db/index.js
@@ -6,11 +6,10 @@ const DB_USER = process.env.DB_USER;
 const DB_PASSWORD = process.env.DB_PASSWORD;
 const DB_HOST = process.env.DB_HOST;
 const DB_DIALECT = process.env.DB_DIALECT;
+const DB_URI = process.env.DB_URI;
 
-const db = new Sequelize(DB_NAME, DB_USER, DB_PASSWORD, {
-  host: DB_HOST,
-  dialect: DB_DIALECT,
-  logging: false,
+const sequelize = new Sequelize(DB_URI, {
+  dialectModule: require("pg"),
 });
 
-module.exports = db;
+module.exports = sequelize;
