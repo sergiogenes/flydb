f85562720dc2db175c0904a6d8e4cbd9d1f72606
sergiogenes <genessergio@gmail.com>
2023-05-16 16:13:00 -0300

    se borraron las env no utilizadas y se acomodó db

--- db/index.js
+++ db/index.js
@@ -1,15 +1,11 @@
 const Sequelize = require("sequelize");
 require("dotenv").config();
 
-const DB_NAME = process.env.DB_NAME;
-const DB_USER = process.env.DB_USER;
-const DB_PASSWORD = process.env.DB_PASSWORD;
-const DB_HOST = process.env.DB_HOST;
-const DB_DIALECT = process.env.DB_DIALECT;
 const DB_URI = process.env.DB_URI;
 
 const sequelize = new Sequelize(DB_URI, {
   dialectModule: require("pg"),
+  logging: false,
 });
 
 module.exports = sequelize;
