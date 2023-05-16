9ece4e8ead20d1693ce2df0c1a9500413fd8c058
sergiogenes <genessergio@gmail.com>
2023-05-16 11:25:56 -0300

    Se adecua el proyecto para deploy en vercel y base de datos en Clever Cloud

--- db/index.js
+++ db/index.js
@@ -2,8 +2,8 @@ const Sequelize = require("sequelize");
 require("dotenv").config();
 
 const DB_NAME = process.env.DB_NAME;
-const DB_USER = JSON.parse(process.env.DB_USER);
-const DB_PASSWORD = JSON.parse(process.env.DB_PASSWORD);
+const DB_USER = process.env.DB_USER;
+const DB_PASSWORD = process.env.DB_PASSWORD;
 const DB_HOST = process.env.DB_HOST;
 const DB_DIALECT = process.env.DB_DIALECT;
 
